1a98377534c3c32774d6e341db93845ee232a07b
Mikhail Goncharov <mgoncharov@merchantry.com>
2013-12-24 11:13:27 +0700

    improved solution for 599 (0.266s -> 0.232s)

--- 599.cpp
+++ 599.cpp
@@ -1,16 +1,9 @@
-#include <algorithm>
 #include <stdio.h>
-#include <iostream>
-#include <cstring>
-#include <vector>
+#include <bitset>
 
 using namespace std;
 
 const int MAX = 26;
-
-// union-disjoint
-int height[MAX];
-int parent[MAX];
 
 char get_token() {
     char c;
@@ -20,55 +13,31 @@ char get_token() {
     return 0;
 }
 
-int get_parent(int a) {
-    while (parent[a] != a) a = parent[a];
-    return a;
-}
-
-void join(int a, int b) {
-    int p, c;
-    a = get_parent(a);
-    b = get_parent(b);
-    if (a == b) return;
-    if (height[a] >= height[b]) {
-        p = a;
-        c = b;
-    } else {
-        p = b;
-        c = a;
-    }
-    if (height[p] == height[c]) height[p]++;
-    parent[c] = p;
-}
-
 int main() {
     int cc;
     scanf("%d", &cc);
     while (cc--) {
-        fill_n(height, MAX, 0);
-        for (int i = 0; i < MAX; i++) parent[i] = i;
-        char a, b;
+        char a;
+        bitset<MAX> connected;
+        int edge_count = 0;
         while (true) {
             a = get_token();
             if (a == '*') break;
-            join(a - 'A', get_token() - 'A');
+            connected.set(a - 'A');
+            connected.set(get_token() - 'A');
+            edge_count++;
         }
         char s[250];
         fgets(s, 250, stdin); // skip asteriks
         fgets(s, 250, stdin);
-        // while (fgets(s, 250, stdin), strlen(s) <= 1) {}
-        int tree_count = 0;
         int acorn_count = 0;
+        int vertex_count = 0;
         for (int i = 0; s[i]; i++) {
             if (s[i] < 'A' || s[i] > 'Z') continue;
-            int j = s[i] - 'A';
-            if (parent[j] != j) continue;
-            if (height[j] == 0) {
-                acorn_count++;
-            } else {
-                tree_count++;
-            }
+            vertex_count++;
+            if (!connected[s[i] - 'A']) acorn_count++;
         }
+        int tree_count = vertex_count - edge_count - acorn_count;
         printf("There are %d tree(s) and %d acorn(s).\n", tree_count, acorn_count);
     }
     return 0;
