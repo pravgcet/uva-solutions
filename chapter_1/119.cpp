f8a1e34fdfc19436b82e04d2c1d4c0b1bbcb6f74
Mikhail Goncharov <mgoncharov@merchantry.com>
2014-02-27 10:01:12 +0700

    chapter 1

---
+++ chapter_1/119.cpp
@@ -0,0 +1,21 @@
+int main() {
+    int n;
+    while (scanf("%d", &n)) {
+        vector<string> names(n);
+        map<string, int> balance;
+        for (int i = 0; i < n; i++) {
+            cin >> names[i];
+            balance[names[i]] = 0;
+        }
+        for (int i = 0; i < n; i++) {
+            string name;
+            int k, amount;
+            cin >> name >> amount >> k;
+            string friend;
+            int amount
+            while (k--) {
+                friend
+            }
+        }
+    }
+}
