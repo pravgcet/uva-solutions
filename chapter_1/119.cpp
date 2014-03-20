600a20a6a289a795f3b88ba19168701fc3202308
Goncharov Mikhail <goncharov.mikhail@gmail.com>
2014-03-20 23:58:50 +0700

    gift givers

--- chapter_1/119.cpp
+++ chapter_1/119.cpp
@@ -1,21 +1,44 @@
+#include <utility>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 int main() {
     int n;
-    while (scanf("%d", &n)) {
-        vector<string> names(n);
-        map<string, int> balance;
+    bool first = true;
+    while (cin >> n) {
+        if (!first) cout << "\n";
+        first = false;
+        vector<string> names;
+        string s;
         for (int i = 0; i < n; i++) {
-            cin >> names[i];
-            balance[names[i]] = 0;
+            cin >> s;
+            names.push_back(s);
+        }
+        map<string, pair<int, int> > balance;
+        for (auto it = names.begin(); it != names.end(); it++) {
+            balance[*it].first = 0;
+            balance[*it].second = 0;
         }
         for (int i = 0; i < n; i++) {
-            string name;
-            int k, amount;
-            cin >> name >> amount >> k;
-            string friend;
-            int amount
-            while (k--) {
-                friend
+            int v, k;
+            cin >> s >> v >> k;
+            if (k > 0) {
+                int a = v / k;
+                balance[s].first -= a * k;
+                for (int j = 0; j < k; j++) {
+                    string p;
+                    cin >> p;
+                    balance[p].second += a;
+                }
             }
         }
+        for (auto i = names.begin(); i != names.end(); i++) {
+            cout << *i << " " << (balance[*i].first + balance[*i].second) << "\n";
+        }
     }
+    return 0;
 }
