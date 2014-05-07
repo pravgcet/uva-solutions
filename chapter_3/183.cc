e65d76d096f4d871ff6ee627f9ea7214423eeb61
Mikhail Goncharov <mgoncharov@merchantry.com>
2014-05-07 16:48:43 +0700

    fixed output of bitmaps

--- chapter_3/183.cc
+++ chapter_3/183.cc
@@ -7,23 +7,45 @@ const int MAX = 200;
 
 bool M[MAX][MAX];
 
-string seq;
-int pos;
 int cols, rows;
+int w;
+
+char next_char() {
+    while (true) {
+        char c = getchar();
+        // cerr << c << ".";
+        switch (c) {
+            case '1':
+            case '0':
+            case 'B':
+            case 'D':
+            case '#':
+            return c;
+        }
+    }
+    return '\n';
+}
+
+void pc(char c) {
+    if (w == 50) {
+        w = 0;
+        putchar('\n');
+    }
+    putchar(c);
+    w++;
+}
 
 void fill_b() {
     for (int r = 0; r < rows; r++) {
         for (int c = 0; c < cols; c++) {
-            M[r][c] = (seq[pos] == '1');
-            pos++;
+            M[r][c] = (next_char() == '1');
         }
     }
 }
 
 void fill_d(int rf, int cf, int rt, int ct) {
     if (rf == rt || cf == ct) return;
-    char q = seq[pos];
-    pos++;
+    char q = next_char();
     if (q == 'D') {
         int rm = (rf + rt + 1) / 2;
         int cm = (cf + ct + 1) / 2;
@@ -49,10 +71,10 @@ void print_d(int rf, int cf, int rt, int ct) {
         }
     }
     if (same) {
-        putchar(M[rf][cf] ? '1' : '0');
+        pc(M[rf][cf] ? '1' : '0');
         return;
     }
-    putchar('D');
+    pc('D');
     int rm = (rf + rt + 1) / 2;
     int cm = (cf + ct + 1) / 2;
     print_d(rf, cf, rm, cm);
@@ -64,26 +86,25 @@ void print_d(int rf, int cf, int rt, int ct) {
 void print_b() {
     for (int r = 0; r < rows; r++) {
         for (int c = 0; c < cols; c++) {
-            putchar(M[r][c] ? '1' : '0');
+            pc(M[r][c] ? '1' : '0');
         }
     }
 }
 
 int main() {
     char t;
-    while(cin >> t, t != '#') {
+    while(t = next_char(), t != '#') {
         scanf("%d%d", &rows, &cols);
-        cin >> seq;
-        pos = 0;
+        // cerr << t << rows << cols << endl;
+        w = 0;
         if (t == 'B') {
             fill_b();
-            printf("D %d %d\n", rows, cols);
+            printf("D%4d%4d\n", rows, cols);
             print_d(0, 0, rows, cols);
             putchar('\n');
         } else {
-            if (t != 'D') break;
             fill_d(0, 0, rows, cols);
-            printf("B %d %d\n", rows, cols);
+            printf("B%4d%4d\n", rows, cols);
             print_b();
             putchar('\n');
         }
