771119929b65825f7a4b356360fdf47a00fb2683
Mikhail Goncharov <mgoncharov@setronica.com>
2015-04-15 17:48:12 +0600

    ez

--- etc/572.cc
+++ etc/572.cc
@@ -12,23 +12,36 @@ const int MAX = 110;
 int main() {
   int m, n;
   int f[MAX][MAX];
-  while (cin >> m, m) {
-    cin >> n;
+  while (cin >> m >> n, m) {
     string s;
     for (int i = 0; i < m; i++) {
       cin >> s;
       for (int j = 0; j < n; j++) {
-        f[i][j] = s[j] == '*' ? 0 : 1;
+        f[i][j] = (s[j] == '*' ? 0 : 1);
       }
     }
     int c = 0;
     for (int i = 0; i < m; i++) {
       for (int j = 0; j < n; j++) {
         if (f[i][j] == 0) continue;
+        c++;
         queue<ii> q;
         q.emplace(i, j);
-        while (q.empty())
+        f[i][j] = 0;
+        while (!q.empty()) {
+          ii p = q.front(); q.pop();
+          for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+              int x = p.first + dx; int y = p.second + dy;
+              if (x < 0 || x >= m || y < 0 || y >= n) continue;
+              if (f[x][y] == 0) continue;
+              q.emplace(x, y);
+              f[x][y] = 0;
+            }
+          }
+        }
       }
     }
+    cout << c << endl;
   }
 }
