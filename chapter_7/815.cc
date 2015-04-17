c2da1d992207bd88dcafe2b08ba004d3d9f40f23
Mikhail Goncharov <mgoncharov@setronica.com>
2015-04-17 12:42:47 +0600

    rain

--- chapter_7/815.cc
+++ chapter_7/815.cc
@@ -13,33 +13,31 @@ int main() {
   while (cin >> m >> n, m) {
     tc++; printf("Region %d\n", tc);
     vector<ll> levels(m * n);
-    ll f = INF;
+    double f = INF;
     for (int i = 0; i < m * n; i++) {
-      cin >> levels[i]; levels[i] *= 100;
-      f = min(f, levels[i]);
+      cin >> levels[i];
+      if (f > levels[i]) f = levels[i];
     }
     ll v; cin >> v;
-    v *= 1000000;
-    ll t = INF; // invariant is f <= x, t > x
-    while (f != t) {
-      ll m = (f + t) / 2;
-      ll a = 0;
+    double t = INF; // invariant is f <= x, t > x
+    while (round(f * 100) != round(t * 100)) {
+      double m = (f + t) / 2;
+      double a = 0;
       for (auto l : levels) {
-        if (m < l) continue;
-        a += 1000 * 1000 * (m - l);
+        if (m > l) a += 100 * (m - l);
       }
-      if (a == v) {
-         f = t = m; break;
+      if (abs(a - v) < 1e-9) {
+        f = t = m; break;
       }
       if (a < v) {
-        f = m + 1;
+        f = m;
       } else {
-        t = m - 1;
+        t = m;
       }
     }
     double flooded = 0;
     for (auto l : levels) if (l < f) flooded++;
-    printf("Water level is %.2f meters.\n", ((double) f) / 100);
+    printf("Water level is %.2f meters.\n", round(f * 100) / 100);
     printf("%.2f percent of the region is under water.\n\n",
           (flooded * 100) / (n * m));
   }
--- wa.md
+++ wa.md
@@ -27,3 +27,4 @@ reasons of WA
 10212 - wrong solution
 10090 - rounding error
 10800 - hasty read of output format
+815 - not so easy to solve with integers because of rounding
