7646fc4e50cb526547ee1f80f6df5dda091e35b4
Mikhail Goncharov <mgoncharov@setronica.com>
2015-04-24 17:58:24 +0600

    draft

--- chapter_7/1206.cc
+++ chapter_7/1206.cc
@@ -9,13 +9,40 @@ const int INF = numeric_limits<int>::max();
 
 struct dot { double x, y; };
 
+struct vec { double x, y;
+  vec(dot a, dot b) {
+    x = b.x - a.x;
+    y = b.y - a.y;
+  }
+};
+
+using polygon = vector<dot>;
+
+void print_polygon(polygon &p) {
+  bool first = true;
+  for (auto d : p) {
+    if (!first) printf(" "); first = false;
+    printf("(%.3f,%.3f)", d.x, d.y);
+  }
+  printf("\n");
+}
+
+polygon build_hull(polygon &p) {
+  polygon hull;
+}
+
 int main() {
   string line;
   while (getline(cin, line)) {
     istringstream ss(line);
-    string p;
-    while (ss >> p) {
-      cout << p << endl;
+    string a;
+    polygon p;
+    while (ss >> a) {
+      dot d;
+      if (sscanf(a.c_str(),"(%lf,%lf)", &d.x, &d.y)) {
+        p.emplace_back(d);
+      }
     }
+    print_polygon(build_hull(p));
   }
 }
