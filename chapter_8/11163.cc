0753371c9b3d20fa4abb0ecb1c409d4ebd18bdc5
Mikhail Goncharov <mgoncharov@setronica.com>
2015-05-21 23:00:49 +0600

    + table lookup

--- chapter_8/11163.cc
+++ chapter_8/11163.cc
@@ -6,7 +6,7 @@ using ii = pair<int,int>;
 using ll = long long;
 using llu = unsigned long long;
 const int INF = numeric_limits<int>::max();
-const int MAX = 40;
+const int MAX = 41;
 
 using vb = vector<int>;
 int n;
@@ -18,7 +18,7 @@ ll ops = 0;
 
 const int di[] = {-4, -3, -1, 1, 3, 4};
 
-map<vi, int> distances;
+int ht[MAX][MAX];
 
 int h() {
   int x = 0;
@@ -39,122 +39,56 @@ int h() {
   return x;
 }
 
-int pdh(int i, int j) {
-  int f = formation[i] - 1;
-  // if (f == 1) return 0;
-  // if (i + 1 % 4 != f[i] % 4) x++;
-  i = abs(i - f);
-  j = abs(j - f);
-  int x = j / 4 - i / 4;
-  i = i % 4;
-  j = j % 4;
-  if (i != j) {
-    // f = f % 4;
-    switch (i) {
-      case 1:
-      case 3: x--; break;
-      case 2: x -= 2; break;
-    }
-    switch (j) {
-      case 1:
-      case 3: x++; break;
-      case 2: x += 2; break;
-    }
-  }
-  return x;
-}
-
 void dfs(int g, int tabu, int hh) {
   // ops++;
   if (hh == 0) {
     result = g;
     return;
   }
-  if (g + hh >= bound) return;
-  // if (hh <= MAX_BACK) {
-  //   auto ff = get_formation();
-  //   if (distances.count(ff)) {
-  //     back_result = min(back_result, distances[ff] + g);
-  //   }
-  // }
+  if (g + hh > bound) return;
   g++;
-  // vector<ii> v;
-  // int m = INF;
-  // for (int i = -4; i <= 4; i++) {
   for (int j = 0; j < 6; j++) {
     int i = di[j];
     if (i == tabu) continue;
     int k = king + i;
     if (k < 0 || k >= n) continue;
     if (j > 0 && j < 5 && k / 4 != king / 4) continue;
-    // int dh = pdh(k, king);
+    int dh = ht[k][formation[k]];
     swap(formation[k], formation[king]); swap(king, k);
-    dfs(g, -i, hh - pdh(k, king));
+    dfs(g, -i, hh - dh + ht[k][formation[k]]);
     swap(formation[k], formation[king]); swap(king, k);
     if (result != INF) return;
   }
 }
-/*
-void fill_back() {
-  queue<pair<vi, int>> q;
-  q.emplace(formation, 0);
-  distances[formation] = 0;
-  while (!q.empty()) {
-    vi f = q.front().first;
-    int d = q.front().second + 1;
-    q.pop();
-    for (int i = 0; i < n; i++) {
-      if (f[i] == 1) {
-        king = i;
-        break;
-      }
-    }
-    for (int i = -4; i <= 4; i++) {
-      if (i == 2 || i == -2 || i == 0) continue;
-      int k = king + i;
-      if (k < 0 || k >= n) continue;
-      if (i < 4 && i > -4) {
-        if (k / 4 != king / 4) continue;
-      }
-      vi a = f;
-      swap(a[k], a[king]);
-      if (distances.count(a) == 0) {
-        distances[a] = d;
-        if (d < MAX_BACK) q.emplace(a, d);
-      }
-    }
-  }
-}
-*/
+
 int ida() {
   bound = h();
   result = INF;
-  back_result = INF;
   int hh = bound;
   for (; result == INF; bound++) {
     dfs(0, 0, hh);
-    // if (result == INF && back_result != INF) result = back_result;
   }
-  // while (result == INF) {
-    // cout << ">" << bound << endl;
-  // }
-  // cout << hh << endl;
   return result;
 }
 
 int main() {
-  // for (int i = 0; i < MAX; i++) formation[i] = i + 1;
-  // king = 0;
-  // for (n = 4; n <= MAX; n += 4) {
-    // fill_back();
-  // }
-  // cerr << "OK" << endl;
-  // return 0;
+  for (int i = 0; i < 40; i++) {
+    for (int f = 2; f <= 40; f++) {
+      int& x = ht[i][f];
+      x = 0;
+      int d = abs(i - f + 1);
+      x += d / 4;
+      switch (d % 4) {
+        case 1:
+        case 3: x++; break;
+        case 2: x += 2; break;
+      }
+    }
+  }
+
   int tc = 0;
   while (cin >> n, n) {
-    // formation.resize(n);
     for (int i = 0; i < n; i++) {
-      // int a; cin >> a;
       cin >> formation[i];
       if (formation[i] == 1) king = i;
     }
