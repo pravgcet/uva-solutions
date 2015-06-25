db4bb4ac2d3482f04cf50f00514a1db4f18f761e
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2015-06-25 21:56:46 +0600

    knights!

--- chapter_9/11643.cc
+++ chapter_9/11643.cc
@@ -42,38 +42,55 @@ ll dp(ll mask, int p) {
 
 int main() {
   ios_base::sync_with_stdio(false); cin.tie(0);
+  const int MAXS = 1100;
+  int space[MAXS][MAXS];
+  fill(&space[0][0], &space[MAXS][0], INF);
+  queue<pair<int, int>> q;
+  space[10][10] = 0;
+  q.emplace(10, 10);
+  while (!q.empty()) {
+    int x0, y0; tie(x0, y0) = q.front(); q.pop();
+    for (auto jj = 0; jj < 8; jj++) {
+      int x = x0 + dx[jj], y = y0 + dy[jj];
+      if (x < 0 || y < 0 || x >= MAXS || y >= MAXS) continue;
+      if (space[x][y] != INF) continue;
+      space[x][y] = space[x0][y0] + 1;
+      q.emplace(x, y);
+    }
+  }
   int tcc; cin >> tcc;
   for (int tc = 1; tc <= tcc; tc++) {
     ll n, s; cin >> s >> n;
-    vector<point> knights(n);
-    for (ll i = 0; i < n; i++) cin >> knights[i].x >> knights[i].y;
+    vector<point> K(n);
     fill(&D[0][0], &D[MAX][0], INF);
-    for (ll i = 0; i < n; i++) {
-      D[i][i] = 0;
-      queue<pair<point, int>> q;
-      q.emplace(make_pair(knights[i], 0));
+    for (ll ii = 0; ii < n; ii++) cin >> K[ii].x >> K[ii].y;
+    for (ll ii = 0; ii < n; ii++) {
+      D[ii][ii] = 0;
+      vvi my(s + 1);
+      for (ll jj = 0; jj < s + 1; jj++) my[jj].resize(s + 1, INF);
+      queue<pair<int, int>> q;
+      my[K[ii].x][K[ii].y] = 0;
+      q.emplace(K[ii].x, K[ii].y);
       while (!q.empty()) {
-        ll d; point p;
-        tie(p, d) = q.front(); q.pop();
-        for (ll j = 0; j < n; j++) {
-
-          ll a = abs(p.x - knights[j].x);
-          ll b = abs(p.y - knights[j].y);
-          if (a < b) swap(a, b);
-          if (a % 2 != 0) continue;
-          if (a / 2 >= b && ((a / 2) % 2 == b % 2)) {
-            D[i][j] = D[j][i] = min(D[i][j], d + a / 2);
-          }
+        int x0, y0; tie(x0, y0) = q.front(); q.pop();
+        int d = my[x0][y0];
+        for (ll jj = 0; jj < 8; jj++) {
+          int x = x0 + dx[jj], y = y0 + dy[jj];
+          if (x < 1 || y < 1 || x > s || y > s) continue;
+          if (my[x][y] != INF) continue;
+          my[x][y] = d + 1;
+          if (d < 3) q.emplace(x, y);
         }
-        if (d < 4) {
-          for (int j = 0; j < 8; j++) {
-            point t;
-            t.x = p.x + dx[j];
-            t.y = p.y + dy[j];
-            if (t.x < 1 || t.x > s || t.y < 1 || t.y > s) continue;
-            q.emplace(make_pair(t, d + 1));
-          }
+      }
+      for (ll jj = ii + 1; jj < n; jj++) {
+        int t = my[K[jj].x][K[jj].y];
+        if (t == INF) {
+          int x1 = K[ii].x, y1 = K[ii].y, x2 = K[jj].x, y2 = K[jj].y;
+          if (x1 > x2) swap(x1, x2);
+          if (y1 > y2) swap(y1, y2);
+          t = space[10 + x2 - x1][10 + y2 - y1];
         }
+        D[ii][jj] = D[jj][ii] = t;
       }
     }
     fill(&dpm[0][0], &dpm[MAX][0], -1);
@@ -81,3 +98,4 @@ int main() {
     cout << "Case " << tc << ": " << answer << endl;
   }
 }
+
